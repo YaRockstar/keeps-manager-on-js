4744b61da6e36c6e26927f6f362423b06288f1a4
Yaroslav Kalashnikov <yekalashnikov@gmail.com>
2022-09-08 18:37:02 +0300

    add several services

--- services/htmlService.js
+++ services/htmlService.js
@@ -1,1 +1,15 @@
-export class HTMLService {}
+export class HTMLService {
+  paintKeepText(keep) {
+    return ``;
+  }
+
+  paintKeep(keep) {
+    return ``;
+  }
+
+  paintKeepList(keepList) {
+    if (keepList.length === 0) {
+      return `<p class="EmptyList">Нет заметок</p>`;
+    }
+  }
+}
---
+++ services/keepService.js
@@ -0,0 +1,8 @@
+export class KeepService {
+  constructor(keepList = [], removedKeepList = []) {
+    this.keepList = keepList;
+    this.removedKeepList = removedKeepList;
+  }
+
+  remove(id) {}
+}
